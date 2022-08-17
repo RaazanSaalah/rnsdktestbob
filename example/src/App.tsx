1709f8e9aa456d23c04e0c23d9f213ad04aeaf19
RaazanSaalah <razansalah@paymob.com>
2022-08-17 10:41:06 +0200

    feat(test): test component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-rnsdktestbob';
+import Index from 'react-native-rnsdktestbob';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  // const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  // React.useEffect(() => {
+  //   multiply(3, 7).then(setResult);
+  // }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result</Text>
+      <Index />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React from 'react';
+import { Text } from 'react-native';
+
+function Index() {
+  return <Text>HI</Text>;
 }
+
+export default Index;
